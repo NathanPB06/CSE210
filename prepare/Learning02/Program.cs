85629b95a4a0d3e23a795f121203b07414756dcc
NathanPB06 <nathanpbrooks@hotmail.com>
2023-11-20 18:44:12 -0500

    Added Abstraction Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2000;
+        job1._endYear = 2024;
+        job1.Display();
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+        resume1._jobs = new List<Job>();
+        resume1._jobs.Add(job1);
+        resume1.Display();
     }
 }
