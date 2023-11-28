87280e5b988e68b6cb453c48139ef34c91d151ad
NathanPB06 <nathanpbrooks@hotmail.com>
2023-11-27 19:59:36 -0500

    added fraction learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+    Fraction fraction1 = new Fraction();
+    Fraction fraction2 = new Fraction(6); 
+    Fraction fraction3 = new Fraction(6, 7);
+    fraction2.SetTop(3);
+    fraction2.SetBottom(4);
+    Console.WriteLine(fraction2.GetBottom());
+    Console.WriteLine(fraction2.GetTop());
+    Console.WriteLine(fraction3.GetFractionString());
+    Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
